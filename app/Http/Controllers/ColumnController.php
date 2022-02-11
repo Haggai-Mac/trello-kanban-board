0d8aedfe3e9cbfc08f1356d2641988a45b1816a5
Haggai Wambua <haggai.wambua@gmail.com>
2022-02-11 20:12:05 +0300

    Added PHP functionality to save/update/delete column

--- app/Http/Controllers/ColumnController.php
+++ app/Http/Controllers/ColumnController.php
@@ -3,60 +3,50 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Arr;
+use App\Models\Column;
 
 class ColumnController extends Controller
 {
     /**
      * Display a listing of the resource.
      *
-     * @return \Illuminate\Http\Response
+     * @return \Illuminate\Http\JsonResponse
      */
     public function index()
     {
-        //
-    }
+        $columns = Column::oldest()->get();
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
+        return response()->json([
+            'columns' => $columns
+        ]);
     }
 
     /**
      * Store a newly created resource in storage.
      *
      * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
+     * @return \Illuminate\Http\JsonResponse
      */
     public function store(Request $request)
     {
-        //
-    }
+        $validator = \Validator::make($request->all(), [
+            'title'=>'required|string',
+        ]);
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
-    {
-        //
-    }
+        if ($validator->fails()) { 
+            return \Response::json(array(
+                'errors' => Arr::flatten($validator->errors()->toArray())
+            ), 422);
+        }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        //
+        $column = new Column();
+        $column->title = $request->title;
+        $column->save();
+
+        return response()->json([
+            'message' => 'Column has been added successfully!'
+        ]);
     }
 
     /**
@@ -64,21 +54,44 @@ class ColumnController extends Controller
      *
      * @param  \Illuminate\Http\Request  $request
      * @param  int  $id
-     * @return \Illuminate\Http\Response
+     * @return \Illuminate\Http\JsonResponse
      */
     public function update(Request $request, $id)
     {
-        //
+        $column = Column::findOrFail($id);
+
+        $validator = \Validator::make($request->all(), [
+            'title'=>'required|string',
+        ]);
+
+        if ($validator->fails()) { 
+            return \Response::json(array(
+                'errors' => Arr::flatten($validator->errors()->toArray())
+            ), 422);
+        }
+
+        $column->title = $request->title;
+        $column->position = $request->position;
+        $column->save();
+
+        return response()->json([
+            'message' => 'Column has been updated successfully!'
+        ]);
     }
 
     /**
      * Remove the specified resource from storage.
      *
      * @param  int  $id
-     * @return \Illuminate\Http\Response
+     * @return \Illuminate\Http\JsonResponse
      */
     public function destroy($id)
     {
-        //
+        $column = Column::findOrFail($id);
+        $column->delete();
+
+        return response()->json([
+            'message' => 'Column has been deleted successfully!'
+        ]);
     }
 }
