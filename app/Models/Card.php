34762250d97087a44690eb5b426b9b75b8122acf
Haggai Wambua <haggai.wambua@gmail.com>
2022-02-11 21:48:56 +0300

    Added accessors tp card model

--- app/Models/Card.php
+++ app/Models/Card.php
@@ -10,6 +10,47 @@ class Card extends Model
     use SoftDeletes;
 
     /**
+     * The accessors to append to the model's array form.
+     *
+     * @var array
+     */
+    protected $appends = [
+        'show_modal',
+        'update_path',
+        'delete_path',
+    ];
+
+    /**
+     * Generates the url path for deleteing the column
+     *
+     * @return {string}
+     */
+    public function getShowModalAttribute()
+    {
+        return false;
+    }
+
+    /**
+     * Generates the url path for updating the card
+     *
+     * @return {string}
+     */
+    public function getUpdatePathAttribute()
+    {
+        return route('api.cards.update', [$this->id]);
+    }
+
+    /**
+     * Generates the url path for deleteing the card
+     *
+     * @return {string}
+     */
+    public function getDeletePathAttribute()
+    {
+        return route('api.cards.destroy', [$this->id]);
+    }
+
+    /**
      * Get the column that owns the card.
      */
     public function column()
