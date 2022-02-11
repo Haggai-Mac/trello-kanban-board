d210b5a3bac599f5aae6c1ca246078573d399db0
Haggai Wambua <haggai.wambua@gmail.com>
2022-02-11 20:13:36 +0300

    Added PHP functionality to store/update/delete cards

--- app/Http/Controllers/CardController.php
+++ app/Http/Controllers/CardController.php
@@ -3,6 +3,9 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Card;
+use App\Models\Column;
+use Illuminate\Support\Facades\DB;
 
 class CardController extends Controller
 {
@@ -30,33 +33,34 @@ class CardController extends Controller
      * Store a newly created resource in storage.
      *
      * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
+     * @return \Illuminate\Http\JsonResponse
      */
     public function store(Request $request)
     {
-        //
-    }
+        $validator = \Validator::make($request->all(), [
+            'title'=>'required|string',
+            'column_id'=>'required|integer',
+            'description'=>'nullable|string',
+            'position'=>'nullable|integer',
+        ]);
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
-    {
-        //
-    }
+        if ($validator->fails()) { 
+            return \Response::json(array(
+                'errors' => Arr::flatten($validator->errors()->toArray())
+            ), 422);
+        }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        //
+        $card = new Card();
+        $card->title = $request->title;
+        $card->column_id = $request->column_id;
+        $card->save();
+
+        $Column = Column::find($request->column_id);
+
+        return response()->json([
+            'cards' => $Column->cards,
+            'message' => 'Card has been added successfully!'
+        ]);
     }
 
     /**
@@ -64,21 +68,49 @@ class CardController extends Controller
      *
      * @param  \Illuminate\Http\Request  $request
      * @param  int  $id
-     * @return \Illuminate\Http\Response
+     * @return \Illuminate\Http\JsonResponse
      */
     public function update(Request $request, $id)
     {
-        //
+        $card = Card::findOrFail($id);
+
+        $validator = \Validator::make($request->all(), [
+            'title'=>'required|string',
+            'column_id'=>'required|integer',
+            'description'=>'nullable|string',
+            'position'=>'nullable|integer',
+        ]);
+
+        if ($validator->fails()) { 
+            return \Response::json(array(
+                'errors' => Arr::flatten($validator->errors()->toArray())
+            ), 422);
+        }
+
+        $card->title = $request->title;
+        $card->description = $request->description;
+        $card->column_id = $request->column_id;
+        $card->position = $request->position;
+        $card->save();
+
+        return response()->json([
+            'message' => 'Card has been updated successfully!'
+        ]);
     }
 
     /**
      * Remove the specified resource from storage.
      *
      * @param  int  $id
-     * @return \Illuminate\Http\Response
+     * @return \Illuminate\Http\JsonResponse
      */
     public function destroy($id)
     {
-        //
+        $card = Card::findOrFail($id);
+        $card->delete();
+
+        return response()->json([
+            'message' => 'Card has been deleted successfully!'
+        ]);
     }
 }
