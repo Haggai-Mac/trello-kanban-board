5afd1c4d5c02cadc4478c9b80a3e8762f02ad00a
Haggai Wambua <haggai.wambua@gmail.com>
2022-02-11 11:01:39 +0300

    Removed versioning of assets

--- webpack.mix.js
+++ webpack.mix.js
@@ -12,5 +12,4 @@ const mix = require('laravel-mix');
  */
 
 mix.js('resources/js/app.js', 'public/js')
-    .sass('resources/scss/app.scss', 'public/css')
-    .version();
+    .sass('resources/scss/app.scss', 'public/css');
