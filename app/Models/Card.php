4f04709e7fdfaf44db72b07c15d0e980d680d1d5
Haggai Wambua <haggai.wambua@gmail.com>
2022-02-11 13:27:47 +0300

    Added relations between columns and cards

--- app/Models/Card.php
+++ app/Models/Card.php
@@ -8,4 +8,12 @@ use Illuminate\Database\Eloquent\SoftDeletes;
 class Card extends Model
 {
     use SoftDeletes;
+
+    /**
+     * Get the column that owns the card.
+     */
+    public function column()
+    {
+        return $this->belongsTo(Column::class);
+    }
 }
--- app/Models/Column.php
+++ app/Models/Column.php
@@ -8,4 +8,37 @@ use Illuminate\Database\Eloquent\SoftDeletes;
 class Column extends Model
 {
     use SoftDeletes;
+
+    /**
+     * The relationships that should always be loaded.
+     *
+     * @var array
+     */
+    protected $with = [
+        'cards',
+    ];
+
+    /**
+     * Get the cards for the column.
+     */
+    public function cards()
+    {
+        return $this->hasMany(Card::class);
+    }
+
+    /**
+     * The "booting" method of the model.
+     *
+     * @return void
+     */
+    protected static function boot()
+    {
+        parent::boot();
+
+        self::deleting(function($column) {
+            $column->cards()->each(function($card) {
+                $card->delete();
+            });
+       });
+    }
 }
